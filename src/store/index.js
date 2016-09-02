980a1e0f713b8e0e4287f9d3ea67307703cd8108
johnrhampton <johnrhampton@gmail.com>
2016-09-02 13:56:23 -0400

    adding store index

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,38 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./configureStore.prod');
-} else {
-  module.exports = require('./configureStore.dev');
-}
+import {createStore, applyMiddleware, compose} from 'redux';
+
+/**
+ * third party middleware
+ */
+import thunkMiddleware from 'redux-thunk';
+
+/**
+ * import root reducer
+ */
+import rootReducer from '../reducers';
+
+/**
+ * create and return the main store
+ */
+export default (initialState) => {
+  /**
+   * baseline middleware used in all environments
+   */
+  const baseline_middlewares = [thunkMiddleware];
+
+  /**
+   * build store for non-production environments
+   */
+  if (process.env.NODE_ENV !== 'production') {
+    const dev_store = require('./dev.store');
+    return dev_store.default(initialState, rootReducer, baseline_middlewares);
+  }
+
+  /**
+   * return production store
+   */
+  return createStore(
+    rootReducer,
+    initialState,
+    compose(applyMiddleware(...baseline_middlewares))
+  );
+};
